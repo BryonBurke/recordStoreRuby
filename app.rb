3346453199e1e9498ef9f61090bf360a811da730
bryon burke <bryonburke@MacBook-Pro.hsd1.or.comcast.net>
2019-12-21 08:31:43 -0800

    rebuild album project

--- app.rb
+++ app.rb
@@ -8,3 +8,81 @@ also_reload('lib/**/*.rb')
 get('/') do
   redirect to('/albums')
 end
+
+get('/albums') do
+  @albums = Album.sort
+  @albums_sold = Album.all_sold
+  erb(:albums)
+end
+
+get('/albums/search') do
+  user_search = params[:search]
+  @search = Album.search(user_search)
+  erb(:search)
+end
+
+
+get ('/albums/new') do
+  erb(:new_album)
+end
+
+post ('/albums') do
+  name = params[:album_name]
+  album = Album.new({:name => name, :id => nil})
+  album.save()
+  redirect to('/albums')
+end
+
+get ('/albums/:id') do
+  @album = Album.find(params[:id].to_i())
+  erb(:album)
+end
+
+get ('/albums/:id/edit') do
+  @album = Album.find(params[:id].to_i())
+  erb(:edit_album)
+end
+
+patch ('/albums/:id') do
+  @album = Album.find(params[:id].to_i())
+  @album.update(params[:name])
+  redirect to('/albums')
+end
+
+get ('/albums/:id/buy') do
+
+    @album = Album.find(params[:id].to_i())
+    @album.sold
+    redirect to('/albums')
+end
+delete ('/albums/:id') do
+  @album = Album.find(params[:id].to_i())
+  @album.delete()
+  redirect to('/albums')
+end
+
+get ('/albums/:id/songs/:song_id') do
+  @song = Song.find(params[:song_id].to_i())
+  erb(:song)
+end
+
+post ('/albums/:id/songs') do
+  @album = Album.find(params[:id].to_i())
+  song = Song.new({:name => params[:song_name], :album_id => @album.id, :id => nil, :artist_name => params[:artist_name], :year_recorded => params[:year_recorded], :studio => params[:studio]})
+  song.save()
+  erb(:album)
+end
+
+patch ('/albums/:id/songs/:song_id') do
+  @album = Album.find(params[:id].to_i())
+  song = Song.find(params[:song_id].to_i())
+  song.update(params[:name], @album.id, params[:artist_name], params[:year_recorded])
+  erb(:album)
+end
+
+delete ('/albums/:id/songs/:song_id') do
+  song = Song.find(params[:song_id].to_i())
+  song.delete
+  @album = Album.find(params[:id].to_i())
+  erb(:album)
+end
